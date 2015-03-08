91af05e3e1a837137b42d9ab9a5541847513823f
Jorge <pakjivega@gmail.com>
2015-03-08 16:12:06 -0700

    second commit

---
+++ src/main/java/com/pakjivega/testing/archiva/app/App.java
@@ -0,0 +1,13 @@
+package com.pakjivega.testing.archiva.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
